b781e758ec42b30e50c5300c6011139a03ed7bab
Kamsi-yonna <etiobikamsi@gmail.com>
2024-04-11 21:57:27 +0100

    feat - Utable funtionality and typesafety

--- app.vue
+++ app.vue
@@ -16,12 +16,27 @@
       />
       <p class="text-lg max-w-[50ch]" v-html="recipe.summary"></p>
     </section>
+
+    <section>
+      <div>
+        <h2>Ingredients</h2>
+        <UTable :rows="recipe.extendedIngredients" :columns="columns"> </UTable>
+      </div>
+    </section>
   </UContainer>
 </template>
 
 <script setup lang="ts">
+import type { InternalApi } from "nitropack";
+
 const { data: recipes } = await useFetch("/api/recipes");
 const recipe = recipes.value![5];
+
+const columnsToShow: Array<
+  keyof InternalApi["/api/recipes"]["get"][number]["extendedIngredients"][number]
+> = ["name"];
+
+const columns = columnsToShow.map((col) => ({ key: col, label: col }));
 </script>
 
 <style scoped></style>
