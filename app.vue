3a21293463e54acd705128a1756b7eef3b7c8662
Kamsi-yonna <etiobikamsi@gmail.com>
2024-04-11 15:21:25 +0100

    chore - fetching and displaying data in browser

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
-  <div><NuxtPage /></div>
+  <div>Hey there</div>
+  <pre>{{ recipe }}</pre>
 </template>
+
+<script setup lang="ts">
+const { data: recipes } = await useFetch("/api/recipes");
+const recipe = recipes.value![0];
+</script>
+
+<style scoped></style>
