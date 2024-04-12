dd266b647c8b9169ec53317e6f6c330cac280467
Kamsi-yonna <etiobikamsi@gmail.com>
2024-04-12 05:33:07 +0100

    chore - switched to method 2

--- providers/spoonacular.ts
+++ providers/spoonacular.ts
@@ -1,28 +1,4 @@
 // import { joinURL } from "ufo";
-import type { ProviderGetImage } from "@nuxt/image";
-import { createOperationsGenerator } from "#image";
-
-const operationsGenerator = createOperationsGenerator();
-
-export const getImage: ProviderGetImage = (
-  src,
-  { modifiers = {}, baseURL = useRuntimeConfig().public.baseURL } = {}
-) => {
-  const sizes = [100, 250, 500];
-  const size = modifiers.width || modifiers.height;
-  const nextBiggest = sizes.find((s) => s >= size!) || sizes[sizes.length - 1];
-  const prefix = `ingredients_${nextBiggest}x${nextBiggest}`;
-
-  const operations = operationsGenerator(modifiers);
-  const url = `${baseURL}/${prefix}${src}`;
-  return {
-    url,
-  };
-};
-// url: joinURL(baseURL, prefix, src),
-
-// ? Method 2, adding the baseUrl link directly.
-// import { joinURL } from "ufo";
 // import type { ProviderGetImage } from "@nuxt/image";
 // import { createOperationsGenerator } from "#image";
 
@@ -30,14 +6,37 @@ export const getImage: ProviderGetImage = (
 
 // export const getImage: ProviderGetImage = (
 //   src,
-//   { modifiers = {}, baseURL = "base url in env file" } = {}
+//   { modifiers = {}, baseURL = useRuntimeConfig().public.baseURL } = {}
 // ) => {
 //   const sizes = [100, 250, 500];
 //   const size = modifiers.width || modifiers.height;
-//   const nextBiggest = sizes.find((s) => s >= size) || sizes[sizes.length - 1];
+//   const nextBiggest = sizes.find((s) => s >= size!) || sizes[sizes.length - 1];
 //   const prefix = `ingredients_${nextBiggest}x${nextBiggest}`;
 
+//   const operations = operationsGenerator(modifiers);
+//   const url = `${baseURL}/${prefix}${src}`;
 //   return {
-//     url: joinURL(baseURL, prefix, src),
+//     url,
 //   };
 // };
+
+// ? Method 2, adding the baseUrl link directly.
+import { joinURL } from "ufo";
+import type { ProviderGetImage } from "@nuxt/image";
+import { createOperationsGenerator } from "#image";
+
+const operationsGenerator = createOperationsGenerator();
+
+export const getImage: ProviderGetImage = (
+  src,
+  { modifiers = {}, baseURL = "https://img.spoonacular.com" } = {}
+) => {
+  const sizes = [100, 250, 500];
+  const size = modifiers.width || modifiers.height;
+  const nextBiggest = sizes.find((s) => s >= size!) || sizes[sizes.length - 1];
+  const prefix = `ingredients_${nextBiggest}x${nextBiggest}`;
+
+  return {
+    url: joinURL(baseURL, prefix, src),
+  };
+};
